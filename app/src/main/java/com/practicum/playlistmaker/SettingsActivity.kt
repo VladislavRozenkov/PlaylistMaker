31fd012b5f39be9ace2abd44d3852e0fce969d83
Vladislav <rozenkov.vladislav@yandex.ru>
2026-02-23 18:49:24 +0300

    Homework 9.2

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,10 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,5 +27,54 @@ class SettingsActivity : AppCompatActivity() {
             finish()
         }
 
+        val share = findViewById<TextView>(R.id.share)
+        share.setOnClickListener {
+            shareApp()
+        }
+
+        val support = findViewById<TextView>(R.id.support)
+        support.setOnClickListener {
+            sendEmailToSupport()
+        }
+
+        val agreement = findViewById<TextView>(R.id.agreement)
+        agreement.setOnClickListener {
+            val offerUrl = getString(R.string.offerUrl)
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(offerUrl))
+            startActivity(intent)
+        }
     }
+
+    private fun shareApp() {
+        val shareText = getString(R.string.shareText)
+        val shareIntent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, shareText)
+        }
+        if (shareIntent.resolveActivity(packageManager) != null) {
+            startActivity(Intent.createChooser(shareIntent, "Поделиться через"))
+        } else {
+            Toast.makeText(this, R.string.toastText, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun sendEmailToSupport() {
+
+        val recipientEmail = getString(R.string.recipientEmail)
+        val subject = getString(R.string.subject)
+        val body = getString(R.string.body)
+
+        val encodedSubject = Uri.encode(subject)
+        val encodedBody = Uri.encode(body)
+        val uri = Uri.parse("mailto:$recipientEmail?subject=$encodedSubject&body=$encodedBody")
+
+        val intentSupport = Intent(Intent.ACTION_SENDTO, uri)
+
+        if (intentSupport.resolveActivity(packageManager) != null) {
+            startActivity(intentSupport)
+        } else {
+            Toast.makeText(this, R.string.toastText, Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
